cc039088d78d12d53e655559c92a3cf93c3fed70
dianaavilaao <165206809+dianaavilaao@users.noreply.github.com>
2024-09-12 20:30:47 -0300

    edits home y login

--- Proyecto_TeLlevoApp_Diana_Nicole/src/app/home/home.page.ts
+++ Proyecto_TeLlevoApp_Diana_Nicole/src/app/home/home.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  name!: string;
 
-  constructor() {}
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.name = params['name'];
+    });
+  }
 }
