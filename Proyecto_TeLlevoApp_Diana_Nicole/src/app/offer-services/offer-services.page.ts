ee7bda321131cc4790eb285dae151cc48f19d196
nicolenavarrorojas <ni.navarror@duocuc.cl>
2024-10-17 23:06:51 -0300

    .

--- Proyecto_TeLlevoApp_Diana_Nicole/src/app/offer-services/offer-services.page.ts
+++ Proyecto_TeLlevoApp_Diana_Nicole/src/app/offer-services/offer-services.page.ts
@@ -9,21 +9,21 @@ import { IonModal } from '@ionic/angular';
   styleUrls: ['./offer-services.page.scss'],
 })
 export class OfferServicesPage implements OnInit {
-  
+
   @ViewChild(IonModal) modal!: IonModal;
   selectedValue: number = 5;
   name!: string;
 
-  constructor(
-    private navController: NavController,
-    
-  ) {
-    
-   }
+  vehiculo: any = {
+    marca: '',
+    modelo: '',
+    patente: '',
+    color: ''
+  };
 
-  ngOnInit() {
-  }
+  constructor(private navController: NavController) {}
 
+  ngOnInit() {}
 
   volver() {
     this.navController.back();
@@ -31,11 +31,35 @@ export class OfferServicesPage implements OnInit {
 
   cancel() {
     this.modal.dismiss(null, 'cancel');
+    this.navController.navigateBack('/offer-services'); 
   }
 
   confirm() {
     this.modal.dismiss(this.name, 'confirm');
+    this.navController.navigateBack('/offer-services'); 
   }
 
+  cancelEdit() {
+    this.modal.dismiss(null, 'cancel');
+    this.navController.navigateBack('/offer-services'); 
+  }
+
+  dismissEditModal() {
+    this.modal.dismiss(null, 'cancel');
+    this.navController.navigateBack('/offer-services'); 
+  }
+
+  confirmEditModal() {
+    this.modal.dismiss(this.vehiculo, 'confirm');
+    this.navController.navigateBack('/offer-services'); 
+  }
+
+  // Nueva función para agregar vehículo QUE!!?¡?!?!?
+  addVehicle() {
+    // Aquí puedes agregar la lógica para almacenar el nuevo vehículo
+    // Ejemplo: guardarlo en una base de datos o en el almacenamiento local
+    this.modal.dismiss(this.vehiculo, 'confirm');
+    this.navController.navigateBack('/offer-services'); // Navega de vuelta
+  }
 
 }
